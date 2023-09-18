7b29bba61ea86cca3d757f643181ca0dffe60d06
Julia Torres <juliadittoro@yahoo.com>
2023-09-17 21:55:27 -0300

    advancing backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,20 @@
 import { fastify } from 'fastify'; 
+import { fastifyCors } from '@fastify/cors';
+import { getAllPrompsRoute } from './routes/get-all-prompts';
+import { upLoadVideoRoute } from './routes/upload-video';
+import { createTranscriptionRoutes } from './routes/create-transcription';
+import { generateAICompletionRoute } from './routes/generate-ai-completion';
 
 const app = fastify(); 
 
-app.get('/', () => {
-    return 'Hello world!';
+app.register(fastifyCors, {
+    origin: '*',
 })
+
+app.register(getAllPrompsRoute); 
+app.register(upLoadVideoRoute); 
+app.register(createTranscriptionRoutes); 
+app.register(generateAICompletionRoute); 
 
 app.listen({
     port: 3333,
